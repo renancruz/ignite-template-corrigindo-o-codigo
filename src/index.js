37566da290cce2bf78d730ee6245a52fe25cd816
Renan Cruz <renan.frcr@gmail.com>
2022-11-26 17:58:27 -0300

    finalizando as correções

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
 
-const { v4: uuid } = require("uuid");
+const { v4: uuid, validate } = require("uuid");
 
 const app = express();
 
 app.use(express.json());
 
 const repositories = [];
+
+function checkIfRepositoryExists(request, response, next){
+  const { id } = request.params
+
+  const repository = repositories.find(repository => repository.id === id)
+
+  if(!repository){
+    return response.status(404).json({error: "Repository not found!"})
+  }
+
+  request.repository = repository
+  next()
+}
 
 app.get("/repositories", (request, response) => {
   return response.json(repositories);
@@ -23,19 +36,24 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
+app.put("/repositories/:id", checkIfRepositoryExists, (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
-
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
+  delete updatedRepository.id
+  delete updatedRepository.likes
+  
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
 
   repositories[repositoryIndex] = repository;
@@ -46,9 +64,13 @@ app.put("/repositories/:id", (request, response) => {
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  if(!validate(id)){
+    return response.status(404).json({error: "Invalid ID!"})
+  }
 
-  if (repositoryIndex > 0) {
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (!repositoryIndex) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -60,15 +82,14 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
   const likes = ++repositories[repositoryIndex].likes;
-
-  return response.json('likes');
+  return response.json({"likes": likes});
 });
 
 module.exports = app;
